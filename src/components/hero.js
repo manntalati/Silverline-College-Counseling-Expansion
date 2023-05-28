fd44a29283befea5d38d6e209100aab2a5169d4d
Mann Talati <mann.talati@gmail.com>
2023-05-27 23:05:40 -0500

    inital commit of hero.js
    rough outline of hero.js - more will be added based on future instruction

---
+++ src/components/hero.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+  return (
+    <header style={{ paddingLeft: 0 }}>
+      <div 
+        className='image-area'
+        style={{ backgroundImage: "url(hero.png)"
+                /*Replace with image of choice*/, 
+                height: 800 }}>
+        <div 
+          className='mask' 
+          style={{ backgroundColor: 'rgba(256, 256, 256)' }}>
+          <div 
+            className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3' 
+                style={{
+        position: 'absolute', left: '50%', top: '0%',
+        transform: 'translate(-50%, -50%)'}}
+                >Silverline Tutoring</h1>
+              <a className='button' href='#!' role='button'style={{
+        position: 'absolute', left: '50%', top: '25%',
+        transform: 'translate(-50%, -50%)'}}>
+                Any call to action needed (button)
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*More will be added based on what is needed*/} 
+    </header>
+    
+        );
+    }
+}
