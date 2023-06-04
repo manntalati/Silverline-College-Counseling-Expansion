ddc48ce240331b9e3a8bed0cb8d4091328050467
just-Muzz <idrisking759@gmail.com>
2023-06-04 19:10:17 +0100

    Partners

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import Reach from "./components/Reach.js";
 import Footer from "./components/Footer.js";
 import Method from "./components/Method/Method";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Partners from "./components/Partners";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<>
-								<Reach />
-								<Footer />
-							</>
-						}
-					/>
-					<Route path="method" element={<Method />} />
-				</Routes>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Reach />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="method" element={<Method />} />
+          <Route path="partners" element={<Partners />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
