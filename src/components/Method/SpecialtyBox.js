40293501c7d87ecf5d1b06e93146750485daa283
Kimi <kimichen1115@gmail.com>
2023-06-04 10:54:00 +0800

    Fixed SpecialtyBox component formatting

--- src/components/Method/SpecialtyBox.js
+++ src/components/Method/SpecialtyBox.js
@@ -10,20 +10,25 @@ const SpecialtyBox = ({ color, title, desc }) => {
 		// 	{text}
 		// </button>
 		<div className="container w-35">
-			<div className="card bg-transparent text-white">
-				<div className="card-body">
-					<h2
-						className="card-title p-2"
+			<div
+				className="col text-left bg-white text-white border-white"
+				style={{ borderRadius: "1em", overflow: "hidden" }}
+			>
+				<div
+					className="row-md bg-danger"
+					// style={{ borderRadius: "20em", overflow: "hidden" }}
+				>
+					<div
+						className="p-1 px-3 my-0"
 						style={{ backgroundColor: color }}
 					>
-						{title}
-					</h2>
-					<p
-						className="card-text text-center"
-						style={{ backgroundColor: "#333638" }}
-					>
-						{desc}
-					</p>
+						<h2 className="text-white">{title}</h2>
+					</div>
+					<div style={{ backgroundColor: "#333638" }}>
+						<p className="p-4 my-0" style={{ fontWeight: 600 }}>
+							{desc}
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
