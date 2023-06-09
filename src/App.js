2ba4cf640acb5b14fc2f7bda931b981fb8a34c8e
Kimi <kimichen1115@gmail.com>
2023-06-09 16:45:09 +0800

    Completed reach and opportunities sections in 'Our Community'.

--- src/App.js
+++ src/App.js
@@ -7,30 +7,32 @@ import Partners from "./components/Partners";
 import Lectures from "./components/Lectures";
 import Reviews from "./components/Reviews";
 import Team from "./components/Team";
+import OurCommunity from "./components/OurCommunity/OurCommunity";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Reach />
-                <Lectures />
-                <Team />
-                <Footer />
-              </>
-            }
-          />
-          <Route path="method" element={<Method />} />
-          <Route path="partners" element={<Partners />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<>
+								<Reach />
+								<Lectures />
+								<Team />
+								<Footer />
+							</>
+						}
+					/>
+					<Route path="method" element={<Method />} />
+					<Route path="partners" element={<Partners />} />
+					<Route path="reviews" element={<Reviews />} />
+					<Route path="community" element={<OurCommunity />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/OurCommunity/OurCommunity.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../Footer";
+import OurReach from "./OurReach";
+import Opportunities from "./Opportunities";
+
+const OurCommunity = () => {
+	return (
+		<>
+			<OurReach />
+			<Opportunities />
+			<Footer />
+		</>
+	);
+};
+
+export default OurCommunity;
