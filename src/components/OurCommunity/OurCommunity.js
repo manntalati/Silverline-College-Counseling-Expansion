3e76679072672e1fcbd8578a07ebcc246f4f8163
kritdass <dasskrit@gmail.com>
2023-06-11 18:26:34 +0530

    Added Quick Help Section to Our Community Page

--- src/components/OurCommunity/OurCommunity.js
+++ src/components/OurCommunity/OurCommunity.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../Footer";
 import OurReach from "./OurReach";
 import Opportunities from "./Opportunities";
+import QuickHelp from "./QuickHelp";
 
 const OurCommunity = () => {
 	return (
 		<>
 			<OurReach />
 			<Opportunities />
+      <QuickHelp />
 			<Footer />
 		</>
 	);
