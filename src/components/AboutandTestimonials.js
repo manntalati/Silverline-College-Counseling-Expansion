77cffe0b416baeb21787fc387a501312bc3527e6
zaydanr <134563317+zaydanr@users.noreply.github.com>
2023-06-12 18:02:35 -0400

    Create AboutandTestimonials.js

---
+++ src/components/AboutandTestimonials.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+class About extends React.Component {
+   render() {
+      return (
+<div>
+    <div class="hero hero2" style={{backgroundImage:'url(about.png)',}}>
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-5 mx-auto text-center">
+					<h1 class="heading mb-5" data-aos="fade-up">Who We Are.</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <div >
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12">
+					<h1 data-aos="fade-up">About Silverline</h1>
+					<h2 class="heading mb-3" data-aos="fade-up" data-aos-delay="100"></h2>
+
+					<p data-aos="fade-up" data-aos-delay="200">Silverline Tutoring is an educational 501(c)(3) nonprofit
+						connecting tutors and students within our organization of over 5000 dedicated members. Founded by
+						college and high school students, this company recognizes the difficulty of learning—we’ve all
+						been there.</p>
+
+					<p data-aos="fade-up" data-aos-delay="300">From the depths of math to complex texts in English,
+						every student has struggled at one point or another. We want to bridge this divide in education
+						by providing free tutoring to any student. But how do we accomplish this difficult task?</p>
+
+					<p data-aos="fade-up" data-aos-delay="400">Silverline creates a free and flexible tutoring service
+						by finding the experts in our community and assigning them to students in need. For instance, if
+						someone has 2+ years of experience in chemistry, they would apply to become a tutor and earn
+						community service hours in exchange for their work. This framework allows us to serve as many
+						students as possible at the cost of nothing while still maintaining quality. </p>
+				</div>
+				<div class="col-lg-1"></div>
+
+		  	</div>
+        </div>
+		<div class="container">
+			<div class="row align-items-center">
+
+				<div class="col-lg-1"></div>
+				<div class="col-lg-12">
+					<h1 data-aos="fade-up">Our Goals</h1>
+					<h2 class="heading mb-3" data-aos="fade-up" data-aos-delay="100"></h2>
+
+					<p data-aos="fade-up" data-aos-delay="200">At Silverline, we want to solve the education divide, one
+						student at a time, by making learning enjoyable and stress-free for all. Therefore, we never
+						hold students accountable to a strict schedule and always work based on the student needs,
+						allowing them to benefit the most. Our tutors are vetted in advance to ensure that they are
+						student-friendly and adaptable. Monthly trivia contests and other games are also held to promote
+						a fun learning environment. We want to set our students up for success while building a strong
+						community that facilitates opportunities for everyone involved. Because of this foundation, our
+						community now serves up to 5000 members.</p>
+				</div>
+
+        </div>
+  </div>
+	
+
+		<div class="container">
+			<div class="row align-items-center">
+
+				<div class="col-lg-1"></div>
+				<div class="col-lg-12">
+					<h1 data-aos="fade-up">Our Target Group</h1>
+					<h2 class="heading mb-3" data-aos="fade-up" data-aos-delay="100"></h2>
+
+					<p data-aos="fade-up" data-aos-delay="200">Silverline is primarily for students in high school.
+						However, our reach extends beyond simply highschoolers. We also offer services to elementary and
+						middle schoolers and college-level students, depending on the course. Our diverse group of
+						tutors allows us to serve a greater population of people. </p>
+
+					<p data-aos="fade-up" data-aos-delay="300">Additionally, aside from students, adults and any other
+						age group are also welcome if they are looking for a refresher in a past course or simply
+						looking to learn something new.</p>
+
+					<p data-aos="fade-up" data-aos-delay="400">In terms of tutors, Silverline accepts anyone from
+						sophomore year of highschool and above. In the past, we have had school teachers helping in our
+						system, meaning that even adults are more than welcome to join our community. Therefore,
+						regardless of age or education, Silverline is an open outlet for all groups whether it be in the
+						form of being a student or a teacher.</p>
+
+
+				</div>
+
+
+			</div>
+
+		</div>
+
+   </div>
+   <div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12">
+					<h1 data-aos="fade-up">Testimonials</h1>
+					<h2 class="heading mb-3" data-aos="fade-up" data-aos-delay="100"></h2>
+
+					<p data-aos="fade-up" data-aos-delay="200">I like how the tutors are very friendly and explain the topics 
+					in a very understanding way. They are quick to respond whenever I have a question. This organization 
+					is great if you want to improve in any subject or if you want to work on a hobby such as chess.</p>
+
+					<p data-aos="fade-up" data-aos-delay="300"> was struggling in math so I requested a tutor. I got so much 
+					help from my tutor and it was free to top it all off. This organization is great for anyone who is 
+					struggling in any subject and wants help from qualified tutors. The self-serving system also works as 
+					although I was previously a student, I'm now the head tutor.</p>
+
+					<p data-aos="fade-up" data-aos-delay="400">Silverline Tutoring is a great organization given the fact 
+					that it is free, has several tutors almost always active, and has a very active and friendly community. 
+					I really enjoy being tutored for free and it helps the tutors because they gain community service hours just 
+					for tutoring you. I would recommend that you join the organization </p>
+					
+					<p data-aos="fade-up" data-aos-delay="500">I was looking for help on computer science and requested a tutor 
+					at Silverline Tutoring. Within a couple of hours I was paired with a tutor and we met the next day over zoom. 
+					My tutor was able to break down the topic and made it really easy for me to understand. He helped me 
+					pass the class with an A+</p>
+
+					<p data-aos="fade-up" data-aos-delay="600">I was looking for quick help from the helpers in the organization, 
+					and I asked for help on a physics question I had and I got a response and help within minutes.</p>
+					
+					<p data-aos="fade-up" data-aos-delay="700">Silverline Tutoring is simply the best, I got so much help on my 
+					homework and the helpers quickly answered all the questions I had </p>
+				</div>
+				<div class="col-lg-1"></div>
+
+		  	</div>
+        </div>
+   </div>
+	
+  );
+  }
+}
+
+export default About;
