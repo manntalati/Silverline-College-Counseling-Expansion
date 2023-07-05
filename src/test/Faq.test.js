71450ae48ee43dc3fc9fef066b0c5734f3343840
just-Muzz <idrisking759@gmail.com>
2023-07-05 08:19:29 +0100

    Moved all test to test folder

--- src/test/Faq.test.js
+++ src/test/Faq.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Faq from '../Faq';
+import Faq from '../components/Faq';
 
 describe('FAQ component', () => {
   test('renders frequently asked questions', () => {
