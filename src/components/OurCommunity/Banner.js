142f99ded12a482448e4216cbde8804fd3e12939
Garv Jain <garv.jainn@gmail.com>
2023-07-05 23:14:43 -0700

    changes to banner css and implementation of discord widget

--- src/components/OurCommunity/Banner.js
+++ src/components/OurCommunity/Banner.js
@@ -2,31 +2,40 @@ import React from "react";
 import bannerImg from "../../images/community/banner.jpg";
 
 const Banner = () => {
-	const titleStyle = {
-		fontSize: "4rem",
-		fontWeight: "bold",
-		color: "white",
-		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -500%)",
-	};
-	return (
-		<>
-			<img
-				src={bannerImg}
-				alt="banner"
-				width="100%"
-				height="700rem"
-				style={{
-					objectPosition: "center 50%",
-					objectFit: "cover",
-					filter: "brightness(50%)",
-				}}
-			/>
-			<h1 style={titleStyle}>Our Community</h1>
-		</>
-	);
+  const bannerStyle = {
+    position: "relative",
+    marginTop: "64px",
+    height: "400px", // Adjust the height as desired
+  };
+
+  const imageStyle = {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    filter: "brightness(50%)",
+  };
+
+  const titleStyle = {
+    fontSize: "4rem",
+    fontWeight: "bold",
+    color: "white",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
+
+  return (
+    <div style={bannerStyle}>
+      <img
+        src={bannerImg}
+        alt="banner"
+        style={imageStyle}
+      />
+      <h1 style={titleStyle}>Our Community</h1>
+    </div>
+  );
 };
 
 export default Banner;
+
--- src/components/OurCommunity/Discord.js
+++ src/components/OurCommunity/Discord.js
@@ -1,34 +1,82 @@
 import React from "react";
 
 const Discord = () => {
+  const containerStyle = {
+    backgroundColor: "#1E2022",
+    padding: "5rem",
+    display: "flex",
+  };
+
+  const leftColumnStyle = {
+    flex: 1,
+  };
+
+  const rightColumnStyle = {
+    flex: 1,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+  };
+
+  const widgetStyle = {
+    width: "400px",
+    height: "500px",
+    border: "none",
+  };
+
   const bodyStyle = {
     fontSize: "1.1rem",
     fontWeight: "bold",
     color: "#D6D3CD",
+    width:"800px",
   };
+
   const titleStyle = {
     fontSize: "2.5rem",
     fontWeight: "bold",
     color: "#918CA1",
   };
+
   return (
-    <div style={{ backgroundColor: "#1E2022", padding: "7rem" }}>
-      <div className="container">
-        <h1 style={titleStyle}>Discord</h1>
-        <p className="my-0" style={bodyStyle}>
-          Silverline has a dedicated Discord server where students can join to connect with our community and access various resources. 
-          In our Discord server, you can find 50+ helpers who are ready to assist you with your questions and provide quick help. Whether 
-          you need help with a specific reading problem, calculus question, or any other academic inquiry, our community helpers are available to support you.
-          <br /><br />
-          To join our Discord server, simply visit our website and click on the Discord invite link. 
-          Once you join, you can indicate your interest in becoming a helper and be assigned to the helper role. 
-          If you have any questions or need assistance, feel free to ask in the appropriate channels, and our community 
-          members will be happy to assist you.
-          <br /><br />
-          Please note that while our Discord server provides quick help for specific questions, for broader and more extensive 
-          tutoring needs, we recommend availing our dedicated tutoring service with our experienced tutors. 
-          Our tutors are available to provide comprehensive guidance and support for various subjects and academic levels.
-        </p>
+    <div style={containerStyle}>
+      <div style={leftColumnStyle}>
+        <div className="container">
+          <h1 style={titleStyle}>Discord</h1>
+          <p className="my-0" style={bodyStyle}>
+            Silverline has a dedicated Discord server where students can join
+            to connect with our community and access various resources. In our
+            Discord server, you can find 50+ helpers who are ready to assist
+            you with your questions and provide quick help. Whether you need
+            help with a specific reading problem, calculus question, or any
+            other academic inquiry, our community helpers are available to
+            support you.
+            <br />
+            <br />
+            To join our Discord server, simply visit our website and click on
+            the Discord invite link. Once you join, you can indicate your
+            interest in becoming a helper and be assigned to the helper role. If
+            you have any questions or need assistance, feel free to ask in the
+            appropriate channels, and our community members will be happy to
+            assist you.
+            <br />
+            <br />
+            Please note that while our Discord server provides quick help for
+            specific questions, for broader and more extensive tutoring needs,
+            we recommend availing our dedicated tutoring service with our
+            experienced tutors. Our tutors are available to provide
+            comprehensive guidance and support for various subjects and academic
+            levels.
+          </p>
+        </div>
+      </div>
+      <div style={rightColumnStyle}>
+        <iframe
+          src="https://discord.com/widget?id=795627064093966348&theme=dark"
+          style={widgetStyle}
+          allowtransparency="true"
+          frameBorder="0"
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
       </div>
     </div>
   );
