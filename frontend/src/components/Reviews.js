e11401769f047f736f267597c17c4c42e34d7d61
Vivaan <vivaan.rajesh2006@gmail.com>
2023-07-18 13:54:42 -0600

    footer and reviews

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
-
+import Footer from "./Footer";
 const Review = () => {
 	const styles = {
 		reviewsPage: {
@@ -1362,7 +1362,9 @@ const Review = () => {
 					<p style={styles.reviewContent}></p>
 				</div>
 			</div>
+			      <Footer />
 		</div>
+		
 	);
 };
 
