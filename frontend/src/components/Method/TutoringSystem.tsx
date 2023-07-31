112a6da4ccb14a668a7ba7c9257869d96010ba2f
Kimi Chen <kimichen1115@gmail.com>
2023-07-31 08:56:03 +0800

    Convert TutoringSystem, Tutors, AboutOurCommunity, and Banner to ts

--- frontend/src/components/Method/TutoringSystem.tsx
+++ frontend/src/components/Method/TutoringSystem.tsx
@@ -1,43 +1,47 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Link } from "react-router-dom";
 
-export default function TutoringSystem() {
-  const containerStyle = {
-    color: "#D6D3CD",
-    position: "relative",
-    fontFamily: "DM Sans, sans-serif",
-  };
+const TutoringSystem: React.FC = () => {
+	const containerStyle: CSSProperties = {
+		color: "#D6D3CD",
+		position: "relative",
+		fontFamily: "DM Sans, sans-serif",
+	};
 
-  const titleStyle = {
-    fontSize: "2.5rem",
-    fontWeight: "bold",
-  };
+	const titleStyle: CSSProperties = {
+		fontSize: "2.5rem",
+		fontWeight: "bold",
+	};
 
-  const bodyStyle = {
-    fontSize: "1.1rem",
-    fontWeight: "bold",
-  };
+	const bodyStyle: CSSProperties = {
+		fontSize: "1.1rem",
+		fontWeight: "bold",
+	};
 
-  return (
-    <div className="container" style={containerStyle}>
-      <h1 className="pt-3" style={titleStyle}>
-        Our Tutoring System
-      </h1>
-      <br></br>
-      <p style={bodyStyle}>
-        Our tutoring system is reliant on high schoolers and college-level
-        students. But how does our system actually function? The first step in
-        our system is signing up. Tutors and students can go to{" "}
-        <Link
-          to="/get-involved"
-          style={{ textDecoration: "none", color: "#5271FF" }}
-        >
-          Get Involved
-        </Link>{" "}
-        and fill out our forms. From there, our system breaks into two paths:
-      </p>
-      <br></br>
-      <br></br>
-    </div>
-  );
-}
+	return (
+		<div className="container" style={containerStyle}>
+			<h1 className="pt-3" style={titleStyle}>
+				Our Tutoring System
+			</h1>
+			<br></br>
+			<p style={bodyStyle}>
+				Our tutoring system is reliant on high schoolers and
+				college-level students. But how does our system actually
+				function? The first step in our system is signing up. Tutors and
+				students can go to{" "}
+				<Link
+					to="/get-involved"
+					style={{ textDecoration: "none", color: "#5271FF" }}
+				>
+					Get Involved
+				</Link>{" "}
+				and fill out our forms. From there, our system breaks into two
+				paths:
+			</p>
+			<br></br>
+			<br></br>
+		</div>
+	);
+};
+
+export default TutoringSystem;
