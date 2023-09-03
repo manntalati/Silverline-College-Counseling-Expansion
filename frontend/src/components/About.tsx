8156139bdbc5f569f36ef388fec415ea63a7e6bc
Vivaan <vivaanrajesh2006@gmail.com>
2023-09-03 14:29:11 -0600

    Update About.tsx

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -45,17 +45,12 @@ const About: React.FC = () => {
               ></h2>
 
               <p data-aos="fade-up" data-aos-delay="200">
-                Silverline Tutoring is an educational 501(c)(3) nonprofit
-                connecting tutors and students within our organization of over
-                5000 dedicated members. Founded by college and high school
-                students, this company recognizes the difficulty of learning—we’ve all been there.
+              Silverline Tutoring is an educational 501(c)(3) nonprofit connecting high school students with enriching and inspiring resources to amplify their education and extracurricular performance within our organization of over 5000 dedicated members. Founded by college and high school students, this company recognizes the difficulty of navigating high school — we’ve all been there.
               </p>
 
               <p data-aos="fade-up" data-aos-delay="300">
-                From the depths of math to complex texts in English, every
-                student has struggled at one point or another. We want to bridge
-                this divide in education by providing free tutoring to any
-                student. But how do we accomplish this difficult task?
+              From the difficulties of finding extracurriculars, to the difficulties of succeeding in them, every student has struggled. We want to provide students with the ability to break past these barriers within extracurricular and educational performance.
+
               </p>
 
               <p data-aos="fade-up" data-aos-delay="400">
@@ -78,17 +73,8 @@ const About: React.FC = () => {
               ></h2>
 
               <p data-aos="fade-up" data-aos-delay="200">
-                At Silverline, we want to solve the education divide, one
-                student at a time, by making learning enjoyable and stress-free
-                for all. Therefore, we never hold students accountable to a
-                strict schedule and always work based on the student needs,
-                allowing them to benefit the most. Our tutors are vetted in
-                advance to ensure that they are student-friendly and adaptable.
-                Monthly trivia contests and other games are also held to promote
-                a fun learning environment. We want to set our students up for
-                success while building a strong community that facilitates
-                opportunities for everyone involved. Because of this foundation,
-                our community now serves up to 5000 members.
+              At Silverline we are passionate about equal opportunity within education, extracurriculars, and admissions. We provide students with a variety of resources - ranging from robotics to Science Olympiad assistance - in order to amplify and enrich their experiences. We want to set our students up for success while building a strong community that facilitates opportunities for everyone involved.
+
               </p>
 
               <h1 style={headingStyle} data-aos="fade-up">
@@ -101,26 +87,15 @@ const About: React.FC = () => {
               ></h2>
 
               <p data-aos="fade-up" data-aos-delay="200">
-                Silverline is primarily for students in high school. However,
-                our reach extends beyond simply highschoolers. We also offer
-                services to elementary and middle schoolers and college-level
-                students, depending on the course. Our diverse group of tutors
-                allows us to serve a greater population of people.
+              Didnt get text for this
               </p>
 
               <p data-aos="fade-up" data-aos-delay="300">
-                Additionally, aside from students, adults and any other age
-                group are also welcome if they are looking for a refresher in a
-                past course or simply looking to learn something new.
+              Didnt get text for this
               </p>
 
               <p data-aos="fade-up" data-aos-delay="400">
-                In terms of tutors, Silverline accepts anyone from the sophomore
-                year of high school and above. In the past, we have had school
-                teachers helping in our system, meaning that even adults are
-                more than welcome to join our community. Therefore, regardless
-                of age or education, Silverline is an open outlet for all groups
-                whether it be in the form of being a student or a teacher.
+              Didnt get text for this
               </p>
             </div>
           </div>
