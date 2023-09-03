d000c85adc8e15d38b150568cab92437ea6007e3
Vivaan <vivaanrajesh2006@gmail.com>
2023-09-03 14:35:31 -0600

    Update AboutOurCommunity.tsx

--- frontend/src/components/OurCommunity/AboutOurCommunity.tsx
+++ frontend/src/components/OurCommunity/AboutOurCommunity.tsx
@@ -23,29 +23,10 @@ const AboutOurCommunity: React.FC = () => {
 					data-aos-delay="100"
 				></h2>
 				<p style={bodyStyle} data-aos="fade-up" data-aos-delay="200">
-					Silverline is filled with students eager to learn. Our
-					communication networks promote healthy discourse between
-					students, allowing for members to find others with similar
-					interests. In this sense, students have the opportunity to
-					connect on a higher level than simply tutoring. They can
-					discuss sports, school, musical interests and any hobbies
-					they have in mind. Additionally, our community is divided
-					into specific categories of interest, such as a debate
-					category that offers healthy discourse surrounding issues
-					such as healthcare but also interesting, light-hearted
-					topics such as movies vs books. Therefore, Silverline also
-					instills a stronger sense of community between members that
-					facilitates a stress-free and positive learning environment
-					for all involved.
+				Silverline is filled with students eager to enrich their educational experiences.. Our communication networks promote healthy discourse between students, allowing for members to find others with similar interests. In this sense, students have the opportunity to connect on a higher level than simply access to resources and advisory. They can discuss sports, school, musical interests and any hobbies they have in mind. Additionally, our community is divided into specific categories of interest. Silverline instills a stronger sense of community between members that facilitates a stress-free and positive learning environment for all involved.
 				</p>
 				<p style={bodyStyle} data-aos="fade-up" data-aos-delay="300">
-					Our community also hosts chess tournaments and trivia
-					contests, presenting all members with ways to unwind and
-					enjoy the community. We understand that learning can be
-					stressful, so we provide a plethora of resources for
-					students in our community to take a breather and relax. Many
-					of these contests often have cash prizes, providing an
-					incentive for all students.
+				We understand that high school can be stressful, so we provide a plethora of resources for students in our community to take a breather and relax. Many of these contests often have prizes, providing an incentive for all students.
 				</p>
 			</div>
 		</div>
